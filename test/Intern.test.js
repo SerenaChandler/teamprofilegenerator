22faff99321f6d01a0283893ee67e47e7bcc21e2
Serena Chandler <serenachandler2019@gmail.com>
2021-04-18 17:47:38 -0700

    added 2 more tests

--- test/Intern.test.js
+++ test/Intern.test.js
@@ -1,1 +1,10 @@
 const Intern = require('../lib/Intern')
+
+    describe('Intern class', () => {
+        it('takes in input and returns a new Intern object', () => {
+        const intern = new Intern("UCLA")
+      
+
+        expect(intern.school).toBe("UCLA")
+        });
+    });
--- test/Manager.test.js
+++ test/Manager.test.js
@@ -1,1 +1,9 @@
 const Manager = require('../lib/Manager')
+
+describe('Manager class', () => {
+    it('takes in input and returns a new Manager object', () => {
+    const manager = new Manager(1137)
+  
+    expect(manager.officeNumber).toBe(1137)
+    });
+});
